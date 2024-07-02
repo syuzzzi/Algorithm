74412a5e4e59f4e06d5c911325f8a2540265fa2f
suji <csj1430@naver.com>
2024-07-03 01:21:17 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,13 @@
+h, m = map(int, input().split())
+
+if m < 45 :
+    if h == 0 :
+        h = 23
+        m = 60 - (45 - m)
+    else :
+        h -= 1
+        m = 60 - (45 - m)
+else :
+    m -= 45
+    
+print(h, m)
