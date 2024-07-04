c18f4081f9f8b1f5380588442059c81b1ba6dcfa
suji <csj1430@naver.com>
2024-07-04 17:36:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 1780 ms, Memory: 318592 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0], min = arr[0];
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
