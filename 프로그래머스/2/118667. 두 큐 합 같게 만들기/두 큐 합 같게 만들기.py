8e722a223c2e489239ed9e75dbe4ead85e60d62e
suji <csj1430@naver.com>
2024-07-28 17:18:11 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 75.14 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -20,22 +20,22 @@ def solution(queue1, queue2):
         
         elif sum1 < sum2 : # 큐1의 합보다 큐2의 합이 크다면
             if deque2 :
-              popped = deque2.popleft()
-              sum2 -= popped
-              deque1.append(popped)
-              sum1 += popped
-              answer += 1
+                popped = deque2.popleft()
+                sum2 -= popped
+                deque1.append(popped)
+                sum1 += popped
+                answer += 1
             else : # deque가 비어 있으면 반복 종료
-              break
+                break
             
         elif sum1 > sum2 : # 큐1의 합보다 큐2의 합이 작다면
             if deque1 :
-              popped = deque1.popleft()
-              sum1 -= popped
-              deque2.append(popped)
-              sum2 += popped
-              answer += 1
+                popped = deque1.popleft()
+                sum1 -= popped
+                deque2.append(popped)
+                sum2 += popped
+                answer += 1
             else : # deque가 비어 있으면 반복 종료
-              break
+                break
         
     return -1
