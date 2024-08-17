fe7807f6262da6be05bae99e1ecc25a80639eea1
suji <csj1430@naver.com>
2024-08-17 16:28:17 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 103.24 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -28,3 +28,43 @@ def solution(arrayA, arrayB):
     
     else :
         return 0
+      
+# 유클리드 호제법 이용한 코드
+'''
+import math
+from functools import reduce
+
+# 배열의 모든 수가 i로 나눠 떨어지지 않는지 확인하는 함수
+def all_no_div(i, nums):
+    return all(num % i != 0 for num in nums)   
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def gcd_of_array(arr):
+    result = arr[0]
+    
+    for num in arr[1:]:
+        result = gcd(result, num)
+        
+    return result
+        
+def solution(arrayA, arrayB):
+    Anum = gcd_of_array(arrayA) # arrayA의 최대공약수
+    Bnum = gcd_of_array(arrayB) # arrayB의 최대공약수
+    
+    # 각 배열의 최소공약수가 존재하고 이 수가 다른 배열의 모든 수를 나눌 수 없을 때
+    if (Anum != 1 and all_no_div(Anum, arrayB)) and (Bnum != 1 and all_no_div(Bnum, arrayA)) :
+      return(max(Anum, Bnum)) # 둘 중에 더 큰 수를 출력
+    
+    elif Anum != 1 and all_no_div(Anum, arrayB) : # 만약 배열A의 최대공약수가 존재하고 이 수가 배열B의 모든 수를 나누지 못한다면
+        return Anum
+        
+    elif Bnum != 1 and all_no_div(Bnum, arrayA) : # 만약 배열B의 최대공약수가 존재하고 이 수가 배열A의 모든 수를 나누지 못한다면
+        return Bnum
+    
+    else :
+        return 0
+'''
