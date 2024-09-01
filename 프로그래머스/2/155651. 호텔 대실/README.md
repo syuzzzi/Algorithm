8b31aae4293a477be8ce71a9a24200267492320a
suji <csj1430@naver.com>
2024-09-01 15:33:21 +0900

    [level 2] Title: 호텔 대실, Time: 42.59 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 37.88 ms
+메모리: 10.6 MB, 시간: 42.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 28일 17:52:36
+2024년 09월 01일 15:33:20
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,29 +1,27 @@
-# 너무너무 어렵당.......... 답을 봐도 잘 이해가 되지 않음...
-
-# book_time 문자열을 시간으로 바꾸는 함수
-def str_to_time(str_time: str) -> int :
-  return int(str_time[0:2]) * 60 + int(str_time[3:]) # 분으로 치환해서 반환해줌
+# 문자열을 시간으로 바꿔주는 함수
+def str_to_time(str_time) :
+    return int(str_time[0:2]) * 60 + int(str_time[3:])
 
 def solution(book_time) :
-  book_times = sorted([[str_to_time(i[0]), str_to_time(i[1]) + 10] for i in book_time])
-  
-  rooms = []
-  
-  # 정수로 변환된 시간에 대하여 반복
-  for book_time in book_times :
-    if not rooms : # 아직 아무 예약도 받지 않았을 때
-      rooms.append(book_time) # 추가해줌
-      
-      continue # 다음 예약으로 넘어감
+    # 예약 시작 시간을 기준으로 정렬하고 예약 종료 시간에는 10분을 더해 새로운 리스트 생성.
+    book_times = sorted([[str_to_time(i[0]), str_to_time(i[1]) + 10] for i in book_time])
     
-    for idx, room in enumerate(rooms) : # enumerate는 인덱스 값을 얻을 수 있음
-      if book_time[0] >= room[-1] : # 다음 예약 시작 시간이 배정된 방의 종료 시간보다 이후라면
-        rooms[idx] = room + book_time # 다음 예약의 종료 시간으로 업데이트
+    rooms = [] # 배정 중인 방을 나타내는 리스트
+    
+    for book_time in book_times:
+        earliest_end_time = float('inf') # 종료 시간이 가장 빠른 방을 찾기
+        chosen_room_index = -1 # 선택한 방의 인덱스
         
-        break
-      
-    # 이 else는 for idx, room .... 과 이어짐. break로 중단되지 않았을 경우 실행.
-    else : # 겹치는 예약 시간이라면
-      rooms.append(book_time) # 추가해줌
-      
-  return len(rooms) # 사용된 방의 개수 출력
+        for idx, room in enumerate(rooms): # enumerate 함수는 인덱스를 반환해줌
+          # 새로 배정할 방의 시작 시간이 이미 배정된 방의 종료 시간보다 이후이고
+          # 이미 배정된 방의 종료 시간이 이미 배정된 방들 중 가장 빨리 끝난다면
+            if book_time[0] >= room[-1] and room[-1] < earliest_end_time:
+                earliest_end_time = room[-1] # 이미 배정된 방들 중 가장 빨리 끝나는 시간 갱신
+                chosen_room_index = idx # 인덱스 갱신(가장 빨리 끝나는 방)
+        
+        if chosen_room_index != -1 : # 방이 선택된 경우, 해당 방의 종료 시간을 갱신
+            rooms[chosen_room_index] = rooms[chosen_room_index] + book_time
+        else : # 배정된 모든 방들과 시간이 겹친다면
+            rooms.append(book_time) # 방추가
+    
+    return len(rooms)
