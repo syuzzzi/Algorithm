a600e15d9d92d1c47c64bdac82a65f50e80e556e
suji <csj1430@naver.com>
2024-09-25 18:49:50 +0900

    [Silver IV] Title: 좋은 단어, Time: 124 ms, Memory: 32688 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.py
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -1,12 +1,9 @@
-# 열품타 9분에 시작
-
 n = int(input())
 good = 0 # 좋은 단어 개수 저장할 변수
 
 for _ in range(n) :
     word = list(input())
     stack = []
-    pairs = 0 # 쌍 개수 저장할 변수
 
     for i in range(len(word)) :
         if not stack : # 스택이 비어 있다면
@@ -14,12 +11,11 @@ for _ in range(n) :
 
         elif stack[-1] == word[i] : # 스택의 탑이 현재 문자와 같다면
             stack.pop()
-            pairs += 1
 
         else :
             stack.append(word[i]) 
 
-    if pairs == len(word) // 2 and not stack :
+    if not stack :
         good += 1
 
 print(good)
