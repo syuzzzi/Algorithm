abff05002a3533d31e237c2daa031602f5a2b569
suji <csj1430@naver.com>
2024-11-04 23:13:21 +0900

    [Silver II] Title: 과자 나눠주기, Time: 3228 ms, Memory: 142104 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split()) # 조카의 수, 과자의 수
+snack_len = list(map(int, input().split()))
+
+l, r = 1, max(snack_len)
+result = 0
+
+while l <= r : # l이 r보다 작거나 같을 동안 반복
+    mid = (l + r) // 2 # l와 r의 중간값인 정수 구해주기. 즉, mid는 최대 과자 길이
+    cnt = 0
+
+    for snack in snack_len : 
+        cnt += snack // mid # 과자들을 mid로 나눈 몫을 더해줌
+
+    if cnt >= m : # m명의 조카들에게 mid 크기로 나눠줄 수 있으면
+        result = mid
+        l = mid + 1 # 더 크게 나눠줄 수 있나 확인
+
+    else : # mid 크기로 나눠줄 수 없다면
+        r = mid - 1 # 더 작은 크기로 나눠줄 수 있나 확인
+
+print(result) # 최대로 나눠줄 수 있는 과자 길이 출력
